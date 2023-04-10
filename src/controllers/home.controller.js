ee0d47d96b63d5d9801ea48b8d93092411e77f0c
markley.sales <markley.sales@lighthouseit.com.br>
2023-04-10 19:19:09 -0300

    correction-heroku2

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,7 +1,5 @@
 async function homeRender (req, res) {
-  res.set('Content-Security-Policy', "default-src 'self'; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; script-src 'self' 'unsafe-inline' https://code.jquery.com https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; img-src 'self' https://i.ytimg.com https://fonts.gstatic.com; connect-src 'self' https://api.github.com/; frame-src https://www.youtube.com/;");
-  res.set('Cross-Origin-Embedder-Policy', 'require-corp');
-  res.set('Cross-Origin-Allow-Origin', 'https://www.youtube.com');
+  res.set('Content-Security-Policy', "default-src 'self'; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; script-src 'self' https://code.jquery.com https://cdnjs.cloudflare.com; style-src 'self' https://fonts.googleapis.com https://cdnjs.cloudflare.com; img-src 'self' https://fonts.gstatic.com;");
   return res.render("home");
 }
 
